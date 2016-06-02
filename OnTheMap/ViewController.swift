523c73f2fbd4d61a3bde6fa4d96d6911f2c48f12
Chris Tran <christopher.huytran@gmail.com>
2016-06-01 23:32:49 -0500

    programmatically add email and password icons to textfields

--- OnTheMap/ViewController.swift
+++ OnTheMap/ViewController.swift
@@ -9,15 +9,40 @@
 import UIKit
 import FlatUIKit
 import ChameleonFramework
+import TextFieldEffects
 
 class ViewController: UIViewController {
 
     //@IBOutlet weak var testButton: FUIButton!
+    
+    @IBOutlet weak var emailTextField: HoshiTextField!
+    @IBOutlet weak var passwordTextField: HoshiTextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.navigationBar.backgroundColor = GradientColor(.TopToBottom, frame: (self.navigationController?.navigationBar.frame)!, colors: [FlatPlumDark(), FlatSkyBlueDark()])
         self.view.backgroundColor = FlatBlueDark()
+        
+        let emailImageView = UIImageView()
+        let emailImage = UIImage(named: "email")
+        emailImageView.image = emailImage
+        emailImageView.frame = CGRect(x: 250, y: 10, width: 20, height: 20)
+        emailTextField.addSubview(emailImageView)
+        let leftView = UIView.init(frame: CGRectMake(10, 0, 30, 30))
+        emailTextField.leftView = leftView
+        emailTextField.leftViewMode = UITextFieldViewMode.Always
+        
+        
+        let pwImageView = UIImageView()
+        let pwImage = UIImage(named: "lock")
+        pwImageView.image = pwImage
+        pwImageView.frame = CGRect(x: 250, y: 10, width: 20, height: 20)
+        passwordTextField.addSubview(pwImageView)
+        let pwleftView = UIView.init(frame: CGRectMake(10, 0, 30, 30))
+        passwordTextField.leftView = pwleftView
+        passwordTextField.leftViewMode = UITextFieldViewMode.Always
+        
         
 //        testButton.buttonColor = UIColor.turquoiseColor()
 //        testButton.shadowColor = UIColor.greenSeaColor()
